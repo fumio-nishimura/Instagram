9e5bbde2d10aa3e49f7eae27e59894a5d7d50f84
fumio-nishimura <cityhunter0522@gmail.com>
2020-08-11 18:35:25 +0900

    Instagram アプリ作成の準備中

---
+++ Instagram/ImageSelectViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageSelectViewController.swift
+//  Instagram
+//
+//  Created by 西村史旺 on 2020/08/11.
+//  Copyright © 2020 Fumio Nishimura. All rights reserved.
+//
+
+import UIKit
+
+class ImageSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Instagram/TabBarController.swift
@@ -0,0 +1,47 @@
+//
+//  TabBarController.swift
+//  Instagram
+//
+//  Created by 西村史旺 on 2020/08/11.
+//  Copyright © 2020 Fumio Nishimura. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // タブアイコンの色
+        self.tabBar.tintColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
+        // タブバーの背景色
+        self.tabBar.barTintColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
+        // UITabBarControllerDelegate プロコトルのメソッドをこのクラスで処理する
+        self.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController is ImageSelectViewController {
+            
+            let imageSelectViewController = storyboard!.instantiateViewController(withIdentifier: "ImageSelect")
+            present(imageSelectViewController, animated: true)
+            return false
+        } else {
+            return true
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
